cffc1dcc5c4f81f93eeab638c72d6208bdd94871
jeonjin <jeonjin616@gmil.com>
2023-09-05 20:38:30 +0900

    0905

--- src/main/java/com/multi/wave/my/MyDAO.java
+++ src/main/java/com/multi/wave/my/MyDAO.java
@@ -6,20 +6,25 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.multi.wave.my.MyVO;
-
 @Repository
 public class MyDAO {
 
 	@Autowired
 	SqlSessionTemplate my;
-	
+
     public List<MyVO> list() {
         return my.selectList("my.list");
     }
-    
+
     public List<MyVO> list(String find) {
         return my.selectList("my.list2", find);
     }
-    
+
+	/*
+	 * public List<MyVO2> list2() { return my.selectList("my.list3"); }
+	 *
+	 * public List<MyVO2> list2(String find2) { return my.selectList("my.list4",
+	 * find2); }
+	 */
+
 }
--- src/main/java/com/multi/wave/my/MyService.java
+++ src/main/java/com/multi/wave/my/MyService.java
@@ -10,12 +10,19 @@ public class MyService {
 
     @Autowired
     MyDAO myDAO;
-    
+
     public List<MyVO> list() {
         return myDAO.list();
     }
-    
+
     public List<MyVO> list(String find) {
         return myDAO.list(find);
     }
-}
+
+/*    public List<MyVO2> list2() {
+        return myDAO.list2();
+    }
+
+    public List<MyVO2> list2(String find2) {
+        return myDAO.list2(find2);*/
+    }
